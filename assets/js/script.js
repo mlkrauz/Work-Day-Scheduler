75e0f7ff9dfe9de5629b27129c9ec2b2221fb42d
ElusiveEntity <30778577+ElusiveEntity@users.noreply.github.com>
2021-06-29 12:10:44 -0400

    Added functions to create and append all html row elements.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,45 +1,64 @@
 
-var timeBlockList = [];
+var scheduleList = [];
 const startHour = 7; //7am
 const endHour = 17; //5pm
 
-function defaultTimeBlocks() {
+function defaultScheduleList() {
     //redeclare the list... just to prevent weird bugs.
-    timeBlockList = [];
+    scheduleList = [];
     
     for (var i = startHour; i <= endHour; i++) {
         //Handle am/pm
         var hour = i;
         var suffix = "AM";
-        if (i >= 13) {
+        if (i > 12) {
             hour = hour - 12;
+            suffix = "PM"
+        } else if (i === 12) {
             suffix = "PM"
         }
 
         time = hour + suffix;
-        timeBlockList.push({hour: time, text: ""});
+        scheduleList.push({hour: time, text: ""});
     }
 }
 
 function getSchedule() {
-    var localTimeBlockList = JSON.parse(localStorage.getItem("timeBlockList"));
+    var localScheduleList = JSON.parse(localStorage.getItem("scheduleList"));
 
     //Is this the first time we're running?
-    if (localTimeBlockList === null) {
-        defaultTimeBlocks();
+    if (localScheduleList === null) {
+        defaultScheduleList();
         setSchedule();
     } else {
-        timeBlockList = localTimeBlockList;
+        scheduleList = localScheduleList;
     }
 }
 
 function setSchedule() {
-    localStorage.setItem("timeBlockList", JSON.stringify(timeBlockList));
+    localStorage.setItem("scheduleList", JSON.stringify(scheduleList));
 }
 
+function createScheduleRowElement(scheduleObject) {
+    //create elements
+    var scheduleRowEl = $(`<div class="row time-block"></div>`);
+    var hourEl = $(`<div class="hour">${scheduleObject.hour}</div>`);
+    var textInputEl = $(`<textarea class="description">${scheduleObject.text}</textarea>`);
+    var saveButtonEl = $('<button class="saveBtn">SAVE</button>');
 
+    //handle classes
 
+    //append and return
+    scheduleRowEl.append(hourEl);
+    scheduleRowEl.append(textInputEl);
+    scheduleRowEl.append(saveButtonEl);
 
+    return scheduleRowEl;
+}
+
+function buttonClickedEvent() {
+
+}
 
 function init() {
     //Set text of the "currentDay" <p> element to today's date.
@@ -47,6 +66,11 @@ function init() {
     $("#currentDay").text(currentDayMoment);
 
     getSchedule();
+
+    //create and append schedule rows
+    for (var i = 0; i < scheduleList.length; i++) {
+        $(".container").append(createScheduleRowElement(scheduleList[i]));
+    }
 }
 
 init()
