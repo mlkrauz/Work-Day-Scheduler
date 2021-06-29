5df87638a882efd2a9ae728d462cc01e4af731c7
ElusiveEntity <30778577+ElusiveEntity@users.noreply.github.com>
2021-06-29 08:29:46 -0400

    Created timeBlockList object, and all local storage handling.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,41 @@
 
+var timeBlockList = [];
+const startHour = 7; //7am
+const endHour = 17; //5pm
 
+function defaultTimeBlocks() {
+    //redeclare the list... just to prevent weird bugs.
+    timeBlockList = [];
+    
+    for (var i = startHour; i <= endHour; i++) {
+        //Handle am/pm
+        var hour = i;
+        var suffix = "AM";
+        if (i >= 13) {
+            hour = hour - 12;
+            suffix = "PM"
+        }
 
+        time = hour + suffix;
+        timeBlockList.push({hour: time, text: ""});
+    }
+}
 
+function getSchedule() {
+    var localTimeBlockList = JSON.parse(localStorage.getItem("timeBlockList"));
+
+    //Is this the first time we're running?
+    if (localTimeBlockList === null) {
+        defaultTimeBlocks();
+        setSchedule();
+    } else {
+        timeBlockList = localTimeBlockList;
+    }
+}
+
+function setSchedule() {
+    localStorage.setItem("timeBlockList", JSON.stringify(timeBlockList));
+}
 
 
 
@@ -9,8 +43,10 @@
 
 function init() {
     //Set text of the "currentDay" <p> element to today's date.
-    var currentDayMoment = moment(new Date()).format("dddd, MMMM Mo, YYYY [ | ] hA");
+    var currentDayMoment = moment().format("LLLL");
     $("#currentDay").text(currentDayMoment);
+
+    getSchedule();
 }
 
 init()
